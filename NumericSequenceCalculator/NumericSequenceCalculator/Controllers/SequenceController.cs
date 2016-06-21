1a95a9d72f33b5ed61d91472fc54bc6aae96af80
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-21 21:49:52 +1000

    Initial commit of basic web application with unit tests

---
+++ NumericSequenceCalculator/NumericSequenceCalculator/Controllers/SequenceController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NumericSequenceCalculator.Controllers
+{
+    public class SequenceController : Controller
+    {
+        // GET: Sequence
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
