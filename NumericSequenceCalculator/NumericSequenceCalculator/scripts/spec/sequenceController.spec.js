ab3c2de319fdfcaa9d1664181cf1b275e5888e2a
suneelvanapalli <suneel.vanapalli@gmail.com>
2016-06-26 12:30:07 +1000

    setting up angular unit testing for controllers and services

---
+++ NumericSequenceCalculator/NumericSequenceCalculator/scripts/spec/sequenceController.spec.js
@@ -0,0 +1,26 @@
+﻿'use strict';
+
+describe("Sequence Controller", function () {
+    var scope;
+    var vm = {
+        init: function () { },
+        generatesequence : function(){}
+    };
+    beforeEach(module('app'));
+     
+
+    beforeEach(inject(function ( $controller,$roootScope) {
+        scope = $roootScope.$new();
+
+        vm = $controller("sequenceController", {
+            $scope : scope
+        });
+    }));
+
+    it('Controller must be defined', function () {
+        expect(vm).toBeDefined();
+    });
+    
+
+
+});
