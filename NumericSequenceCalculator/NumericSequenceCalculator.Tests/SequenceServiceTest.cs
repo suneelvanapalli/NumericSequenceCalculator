3681e08752e8f614da079fd94bbc2f18e3e58ede
suneelvanapalli <suneel.vanapalli@gmail.com>
2017-02-01 21:42:15 +1100

    removed deleted files, upated unit test to nunit

--- NumericSequenceCalculator/NumericSequenceCalculator.Tests/SequenceServiceTest.cs
+++ NumericSequenceCalculator/NumericSequenceCalculator.Tests/SequenceServiceTest.cs
@@ -1,23 +1,23 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NumericSequenceCalculator.BusinessServices;
 using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace NumericSequenceCalculator.Tests
 {
-    [TestClass]
+    [TestFixture]
     public class SequenceServiceTest
     {
-        [TestMethod]
+        [Test]
         public void GetAllNumbersTest()
         {
             var service = new SequenceService();
             var numbers = service.GetAllNumbers(5);
-
-            Assert.AreEqual(6, numbers.Count);
+             
+            Assert.AreEqual(5, numbers.Count);
         }
 
-        [TestMethod]
+        [Test]
         public void GetAllOddNumbersTest()
         {
             var service = new SequenceService();
@@ -26,7 +26,7 @@ namespace NumericSequenceCalculator.Tests
             Assert.AreEqual(3, numbers.Count);
         }
 
-        [TestMethod]
+        [Test]
         public void GetAllEvenNumbersTest()
         {
             var service = new SequenceService();
@@ -34,12 +34,11 @@ namespace NumericSequenceCalculator.Tests
             //2,4
 
             Assert.AreEqual(2, numbers.Count);
-            Assert.IsInstanceOfType(numbers, typeof(List<int>));
+            Assert.IsInstanceOf<List<int>>(numbers);
 
         }
 
-        [TestMethod]
-        [Description("To test count of numbers in sequence for all numbers sequence")]
+        [Test(Description = "To test count of numbers in sequence for all numbers sequence")]
         public void GetAllNumbersCountTest()
         {
             var service = new SequenceService();
@@ -49,8 +48,7 @@ namespace NumericSequenceCalculator.Tests
             Assert.AreEqual(11, numbers.Count);
         }
 
-        [TestMethod]
-        [Description("To verify whether it is including the input number")]
+        [Test( Description = "To verify whether it is including the input number")]
         public void GetEvenCountTest()
         {
             var service = new SequenceService();
@@ -59,39 +57,39 @@ namespace NumericSequenceCalculator.Tests
             Assert.AreEqual(10, numbers[numbers.Count-1]);
         }
 
-        [TestMethod]
-        [Description("To verify that it is including the input fibonacci number as well")]
-        public void GetFibonacciSequenceTest()
-        {
-            var service = new SequenceService();
-            var numbers = service.GetAllFibonacciNumbers(13);
+        //[TestMethod]
+        //[Description("To verify that it is including the input fibonacci number as well")]
+        //public void GetFibonacciSequenceTest()
+        //{
+        //    var service = new SequenceService();
+        //    var numbers = service.GetAllFibonacciNumbers(13);
 
-            Assert.AreEqual(13, numbers[numbers.Count - 1]);
-        }
+        //    Assert.AreEqual(13, numbers[numbers.Count - 1]);
+        //}
 
-        [TestMethod]
-        public void GetNumbersInCustomFormat()
-        {
-            var service = new SequenceService();
-            var numbers = service.GetNumbersInCustomFormat(5);
-
-
-            Assert.AreEqual(5, numbers.Count);
-            Assert.IsInstanceOfType(numbers, typeof(List<string>));
-
-        }
+        //[TestMethod]
+        //public void GetNumbersInCustomFormat()
+        //{
+        //    var service = new SequenceService();
+        //    var numbers = service.GetNumbersInCustomFormat(5);
 
 
-        [TestMethod]
-        public void GetAllFibonacciNumbers()
-        {
-            var service = new SequenceService();
-            var numbers = service.GetAllFibonacciNumbers(10);
+        //    Assert.AreEqual(5, numbers.Count);
+        //    Assert.IsInstanceOfType(numbers, typeof(List<string>));
+
+        //}
 
 
-            //Assert.AreEqual(5, numbers.Count);
-            Assert.IsInstanceOfType(numbers, typeof(List<int>));
+        //[TestMethod]
+        //public void GetAllFibonacciNumbers()
+        //{
+        //    var service = new SequenceService();
+        //    var numbers = service.GetAllFibonacciNumbers(10);
 
-        }
+
+        //    //Assert.AreEqual(5, numbers.Count);
+        //    Assert.IsInstanceOfType(numbers, typeof(List<int>));
+
+        //}
     }
 }
